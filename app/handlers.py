3d65d83145ed105bec6dcb3aa0c05f8777655cf1
kbaafi <kbaafi@gmail.com>
2026-05-16 19:50:44 +0300

    codecrafters submit [skip ci]

--- app/handlers.py
+++ app/handlers.py
@@ -1,4 +1,5 @@
 from abc import ABC, abstractmethod
+import os
 
 
 class AbstractResult(ABC):
@@ -46,7 +47,19 @@ def type_handler(*args):
     if queried_command in built_ins:
         return StringResult(f'{queried_command} is a shell builtin')
     else:
-        return StringResult(f'{queried_command}: not found')
+        return executable_file_handler(args)
+    
+
+def executable_file_handler(*args):
+    command = args[0]
+    path = os.environ["PATH"]
+    dirs = path.split(":")
+
+    for dir in dirs:
+        full_path = os.path.join(dir)
+        if os.path.isfile(full_path) and os.access(full_path, os.X_OK):
+            return StringResult(f'{command} is {full_path}')
+    return StringResult(f'{command}: not found')
 
 
 built_ins = {
