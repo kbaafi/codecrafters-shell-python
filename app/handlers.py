99fd03d0f7b12828e83e0455b2c78f45bfabaa69
kbaafi <kbaafi@gmail.com>
2026-05-16 22:53:27 +0300

    codecrafters submit [skip ci]

--- app/handlers.py
+++ app/handlers.py
@@ -1,5 +1,10 @@
 from abc import ABC, abstractmethod
+from enum import Enum
 import os
+from typing import Generic, TypeVar
+import subprocess
+
+T = TypeVar("T")
 
 
 class AbstractResult(ABC):
@@ -31,39 +36,61 @@ class StringResult(BaseResult):
         super().__init__(result)
 
 
+class Result(Generic[T]):
+    def __init__(self, value: T, interrupt: bool = False) -> None:
+        self._value = value
+        self._interrupt = interrupt
+
+    @property
+    def value(self) -> T:
+        return self._value
+    
+    @property
+    def interrupt(self) -> bool:
+        return self._interrupt
+    
+
+# class CommandType(Enum):
+#     BUILTIN = "BUILTIN"
+#     EXECUTABLE = "EXECUTABLE"
+    
+
 
 def exit_handler(*args):
     _ = args
-    return ExitResult(None)
+    return Result[None](value=None, interrupt=True)
 
 
 def echo_handler(*args):
     result_msg = f'{" ".join(args)}'
-    return StringResult(result_msg)
+    return Result[str](value=result_msg)
 
 
-def type_handler(*args):
-    queried_command = str(args[0])
-    if queried_command in built_ins:
-        return StringResult(f'{queried_command} is a shell builtin')
-    else:
-        return executable_file_handler(*args)
+# def type_handler(*args):
+#     queried_command = str(args[0])
+#     ctype: CommandType = CommandType.EXECUTABLE
+#     if queried_command in built_ins:
+#         ctype = CommandType.BUILTIN
+#     return Result[CommandType](value=ctype)
     
 
-def executable_file_handler(*args):
-    command = args[0]
-    path = os.environ["PATH"]
-    dirs = path.split(os.pathsep)
+# def executable_file_handler(*args):
+#     command = args[0]
+#     path = os.environ["PATH"]
+#     dirs = path.split(os.pathsep)
 
-    for dir in dirs:
-        full_path = os.path.join(dir, command)
-        if os.path.isfile(full_path) and os.access(full_path, os.X_OK):
-            return StringResult(f'{command} is {full_path}')
-    return StringResult(f'{command}: not found')
+#     for dir in dirs:
+#         full_path = os.path.join(dir, command)
+#         if os.path.isfile(full_path) and os.access(full_path, os.X_OK):
+#             return StringResult(f'{command} is {full_path}')
+#     return StringResult(f'{command}: not found')
+
+def run_executable(command: str, *args):
+    result = subprocess.run([command, *args])
+    return Result[subprocess.CompletedProcess](value=result)
 
 
 built_ins = {
     "exit": exit_handler,
     "echo": echo_handler,
-    "type": type_handler,
 }
--- app/main.py
+++ app/main.py
@@ -1,6 +1,34 @@
+from enum import Enum
 import sys
 from .common import PROMPT
-from .handlers import built_ins, BaseResult
+from .handlers import built_ins, run_executable
+import os
+
+
+class CommandType(Enum):
+    BUILTIN = "BUILTIN"
+    EXECUTABLE = "EXECUTABLE"
+    INVALID = "INVALID"
+
+
+def is_executable_command(command):
+    path = os.environ["PATH"]
+    dirs = path.split(os.pathsep)
+
+    for dir in dirs:
+        full_path = os.path.join(dir, command)
+        if os.path.isfile(full_path) and os.access(full_path, os.X_OK):
+            return True
+    return False
+
+
+def ascertain_command_type(command: str) -> CommandType:
+    if command in built_ins:
+        return CommandType.BUILTIN
+    elif is_executable_command(command):
+        return CommandType.EXECUTABLE
+    return CommandType.INVALID
+
 
 def main():
     while True:
@@ -11,15 +39,23 @@ def main():
         command = user_input.strip().split()[0]
         args = user_input.strip().split()[1:]
 
-        if command not in built_ins:
-            print(f"{command}: command not found")
-        else:
-            result: BaseResult = built_ins[command](*args)
-            if result.interrupt:
-                break
-            else:
-                print(result.result)
 
+        command_type = ascertain_command_type(command)
+
+        if command_type == CommandType.BUILTIN:
+            result = built_ins[command](*args)
+        elif command_type == CommandType.EXECUTABLE:
+            result = run_executable(command, *args)
+        else:
+            print(f"{command}: command not found")
+            continue
+
+        # Handle results
+        if result.interrupt:
+            break
+        elif isinstance(result.value, str):
+            print(result.value)
+        
 
 if __name__ == "__main__":
     main()
