bcda4ef6c2505888d461654db13d8e6eb497262c
kbaafi <kbaafi@gmail.com>
2026-05-16 18:24:42 +0300

    codecrafters submit [skip ci]

--- app/handlers.py
+++ app/handlers.py
@@ -1,4 +1,35 @@
 from .results import ExitResult, StringResult
+from abc import ABC, abstractmethod
+
+
+class AbstractResult(ABC):
+    @property
+    @abstractmethod
+    def result(self) -> str: ...
+
+
+class BaseResult(AbstractResult):
+    _interrupt: bool = False
+    def __init__(self, result: str):
+        self._result = result
+
+    @property
+    def result(self) -> str:
+        return self._result
+    
+    @property
+    def interrupt(self) -> bool:
+        return self._interrupt
+    
+
+class ExitResult(BaseResult):
+    _interrupt: bool = True
+
+
+class StringResult(BaseResult):
+    def __init__(self, result: str):
+        super().__init__(result)
+
 
 
 def exit_handler(*args):
