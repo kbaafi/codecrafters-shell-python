b124c841b14fed54623f89ccc7771925ed17b1dc
kbaafi <kbaafi@gmail.com>
2026-05-16 17:23:53 +0300

    codecrafters submit [skip ci]

--- app/handlers.py
+++ app/handlers.py
@@ -1,2 +1,7 @@
-def exit_handler():
+def exit_handler(*args):
+    _ = args
     return -1
+
+def echo_handler(*args):
+    print(" ".join(str(args)))
+    print("\n")
--- app/main.py
+++ app/main.py
@@ -1,24 +1,24 @@
 import sys
-from .handlers import exit_handler
+from .handlers import exit_handler, echo_handler
 
 
 PROMPT = "$ "
-
-
-
-
-
-dispatch = {"exit": exit_handler}
+dispatch = {
+    "exit": exit_handler,
+    "echo": echo_handler,
+}
 
 
 def main():
     while True:
         user_input = input(PROMPT)
         command = user_input.strip().split()[0]
+        args = user_input.strip().split()[1:]
+
         if command not in dispatch:
             print(f"{command}: command not found")
         else:
-            result = dispatch[command]()
+            result = dispatch[command](*args)
             if result == -1:
                 break
 
