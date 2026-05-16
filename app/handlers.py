d43ae9e083df6438d7f11defc969871bafc537ff
kbaafi <kbaafi@gmail.com>
2026-05-16 22:55:20 +0300

    codecrafters submit [skip ci]

--- app/handlers.py
+++ app/handlers.py
@@ -7,35 +7,6 @@ import subprocess
 T = TypeVar("T")
 
 
-class AbstractResult(ABC):
-    @property
-    @abstractmethod
-    def result(self) -> str: ...
-
-
-class BaseResult(AbstractResult):
-    _interrupt: bool = False
-    def __init__(self, result: str):
-        self._result = result
-
-    @property
-    def result(self) -> str:
-        return self._result
-    
-    @property
-    def interrupt(self) -> bool:
-        return self._interrupt
-    
-
-class ExitResult(BaseResult):
-    _interrupt: bool = True
-
-
-class StringResult(BaseResult):
-    def __init__(self, result: str):
-        super().__init__(result)
-
-
 class Result(Generic[T]):
     def __init__(self, value: T, interrupt: bool = False) -> None:
         self._value = value
@@ -50,11 +21,6 @@ class Result(Generic[T]):
         return self._interrupt
     
 
-# class CommandType(Enum):
-#     BUILTIN = "BUILTIN"
-#     EXECUTABLE = "EXECUTABLE"
-    
-
 
 def exit_handler(*args):
     _ = args
@@ -65,25 +31,6 @@ def echo_handler(*args):
     result_msg = f'{" ".join(args)}'
     return Result[str](value=result_msg)
 
-
-# def type_handler(*args):
-#     queried_command = str(args[0])
-#     ctype: CommandType = CommandType.EXECUTABLE
-#     if queried_command in built_ins:
-#         ctype = CommandType.BUILTIN
-#     return Result[CommandType](value=ctype)
-    
-
-# def executable_file_handler(*args):
-#     command = args[0]
-#     path = os.environ["PATH"]
-#     dirs = path.split(os.pathsep)
-
-#     for dir in dirs:
-#         full_path = os.path.join(dir, command)
-#         if os.path.isfile(full_path) and os.access(full_path, os.X_OK):
-#             return StringResult(f'{command} is {full_path}')
-#     return StringResult(f'{command}: not found')
 
 def run_executable(command: str, *args):
     result = subprocess.run([command, *args])
