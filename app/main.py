7f1dac68f53dbb9a9410c70f15e87ce7fed8dfac
kbaafi <kbaafi@gmail.com>
2026-05-21 05:40:44 +0300

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -41,7 +41,7 @@ def main():
                 with open(redirect_file, 'w') as file:
                     file.write(result.value)
             else:
-                print(result.value)
+                print(result.value, end='')
         
 
 if __name__ == "__main__":
