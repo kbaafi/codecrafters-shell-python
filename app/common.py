392480bf46c13089d2ce3a863cc2499f3e870489
kbaafi <kbaafi@gmail.com>
2026-05-21 04:15:27 +0300

    codecrafters submit [skip ci]

--- app/common.py
+++ app/common.py
@@ -1,15 +1,16 @@
 import os
 from typing import Union
-from enum import Enum
+from enum import Enum, auto
 
 
 PROMPT = "$ "
 
 
 class CURSOR_STATE(Enum):
-    IN_QUOTE = "IN_QUOTE"
-    OUT_QUOTE = "OUT_QUOTE"
-    ESCAPE = "ESCAPE"
+    IN_QUOTE = auto()
+    OUT_QUOTE = auto()
+    ESCAPE = auto()
+    IN_QUOTE_ESCAPE = auto()
 
 
 def is_executable_command(command) -> tuple[bool, Union[str, None]]:
@@ -21,52 +22,6 @@ def is_executable_command(command) -> tuple[bool, Union[str, None]]:
         if os.path.isfile(full_path) and os.access(full_path, os.X_OK):
             return True, full_path
     return False, None
-
-
-# def tokenize_args(input_str: str) -> list[str]:
-#     tokens = []
-#     current = []
-#     quote_char = None
-#     escaped = False
-#     in_quote = False
-#     cusor_state: CURSOR_STATE = CURSOR_STATE.OUT_QUOTE
-
-#     for ch in input_str:
-#         if ch in ("'", '"') and quote_char is None:
-#             quote_char = ch
-#             cusor_state = CURSOR_STATE.IN_QUOTE
-#         elif cusor_state==CURSOR_STATE.IN_QUOTE and ch == quote_char:
-#             quote_char = None
-#             cusor_state = CURSOR_STATE.OUT_QUOTE
-#         else:
-
-        
-#         if in_quote:
-#             if ch = qu
-#             current.append(ch)
-#         else:
-#             if ch == "\\": 
-        
-
-#         elif ch == " " and quote_char is None:
-#             if current:
-#                 tokens.append("".join(current))
-#                 current = []
-#         elif ch == "\\":
-#             escaped = True
-#             if current:
-#                 tokens.append("".join(current))
-#             current = []
-#         elif escaped:
-#             current.append(ch)
-#             escaped = False
-#             tokens.append("".join(current))
-#             current = []
-#         else:
-#             current.append(ch)
-#     if current:
-#         tokens.append("".join(current))
-#     return tokens
 
 
 def tokenize_args(input_str: str) -> list[str]:
@@ -98,12 +53,18 @@ def tokenize_args(input_str: str) -> list[str]:
                 if ch == quote_char:
                     quote_char = None
                     state = CURSOR_STATE.OUT_QUOTE
+                elif quote_char == '"' and ch == "\\":
+                    state = CURSOR_STATE.IN_QUOTE_ESCAPE
                 else:
                     current.append(ch)
 
             case CURSOR_STATE.ESCAPE:
                 current.append(ch)
                 state = CURSOR_STATE.OUT_QUOTE
+
+            case CURSOR_STATE.IN_QUOTE_ESCAPE:
+                current.append(ch)
+                state = CURSOR_STATE.IN_QUOTE
     if current:
         tokens.append("".join(current))
 
