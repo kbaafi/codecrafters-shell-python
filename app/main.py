450092f1985d4c036cbbc4ecc5e2ffea6a882451
kbaafi <kbaafi@gmail.com>
2026-05-22 16:20:24 +0300

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -46,7 +46,7 @@ def main():
         elif stdout_redirect is not None:
             with open(stdout_redirect, 'w') as file:
                 file.write(result.value or "")
-        elif stdout_redirect is None and result.value:
+        elif (stdout_redirect is None or stderr_redirect is None) and result.value:
             output = result.value if result.value.endswith('\n') else result.value + '\n'
             sys.stdout.write(output)
         
