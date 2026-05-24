b231aaa4ee7a2d8dd8555b0353643406ec859660
kbaafi <kbaafi@gmail.com>
2026-05-24 21:57:46 +0300

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -59,7 +59,8 @@ def make_completer(shell: Shell):
                     )
                 except OSError:
                     cache = {"options": [], "text": ""}
-        return cache["options"][0] + "s" if len(cache["options"]) > 0 else None
+        print(len(cache["options"]))
+        return cache["options"][0] if len(cache["options"]) > 0 else None
 
         # if len(cached["options"]) == 0:
         #     return None
