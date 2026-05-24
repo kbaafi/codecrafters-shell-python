c90aa91a2bdeccd3584c2c53340af8f160c10883
kbaafi <kbaafi@gmail.com>
2026-05-24 14:00:09 +0300

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -69,7 +69,7 @@ def make_completer(shell: Shell):
                 return None
             else:
                 sys.stdout.write("\n" + "  ".join(sorted(cached["options"])) + "\n")
-                sys.stdout.write("\n" + text + "\n")
+                sys.stdout.write("\n" + tab_count["last_text"] + "\n")
                 sys.stdout.flush()
                 readline.redisplay()
                 # return tab_count["last_text"]
