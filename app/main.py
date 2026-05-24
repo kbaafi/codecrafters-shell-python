02a847cdc171e62e2cf324036dfcb5253f485b74
kbaafi <kbaafi@gmail.com>
2026-05-24 10:40:53 +0300

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -62,10 +62,10 @@ def make_completer(shell: Shell):
         if state == 1:
             prefix = cached["text"] or ""
             display = [o.removeprefix(prefix) for o in cached["options"]]
-            sys.stdout.write("  ".join(display) + "\n")
+            # sys.stdout.write("  ".join(display) + "\n")
             sys.stdout.flush()
             # readline.redisplay()
-            return None
+            return "  ".join(display) + "\n"
         return None
 
     return completer
