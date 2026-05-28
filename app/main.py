ad3db04005e3d1d8f0274bed80dc13ae606c7c40
kbaafi <kbaafi@gmail.com>
2026-05-28 10:38:59 +0300

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -64,7 +64,7 @@ def make_completer(shell: Shell):
                 sys.stdout.flush()
                 return None
             if cache["tab_count"] > 1:
-                out = " ".join(cache["options"]) + "\n"
+                out = " ".join(cache["options"])
                 sys.stdout.write(out)
                 sys.stdout.flush()
                 return ""
