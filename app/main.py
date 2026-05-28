32bf46cffe29b543a253d0410577811560d3fc8f
kbaafi <kbaafi@gmail.com>
2026-05-28 20:41:21 +0300

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -67,7 +67,7 @@ def make_completer(shell: Shell):
                 return None
             if state == 0 and len(options) > 0 and cache["tab_count"] > 0:
                 cache["tab_count"] += 1
-                return " ".join(options)
+                return None
 
             # if text != cache["text"]:
             #     cache["tab_count"] = 0
