85a2e6d4e0c0675bbf53fa1af1d969cf2be883ce
kbaafi <kbaafi@gmail.com>
2026-06-07 20:38:29 +0300

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -61,8 +61,9 @@ def make_completer(shell: Shell):
         #     #         return None
         #     # return " ".join(options)
         # else:
-        print("argstr:", argstr)
+
         base_dir, partial_file = argstr.rsplit("/", 1)
+        print("argstr:", argstr)
         print("base_dir:", base_dir)
         print("partial_file:", partial_file)
         resolved_dir = (
