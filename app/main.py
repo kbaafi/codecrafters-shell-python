868484baeb19623d64e41bbf56409fad9819f6fd
kbaafi <kbaafi@gmail.com>
2026-06-07 17:52:54 +0300

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -109,6 +109,7 @@ def make_completer(shell: Shell):
         if state == 0 and len(options) > 1:
             sys.stdout.write("\a")
             sys.stdout.flush()
+            return None
         return options[state] if state < len(options) else None
 
     return completer
