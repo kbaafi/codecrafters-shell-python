6c7f137364c37f09d7a44af985bb0c2f48e372e3
kbaafi <kbaafi@gmail.com>
2026-06-07 20:44:43 +0300

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -37,11 +37,11 @@ def make_completer(shell: Shell):
         # command, argstr = (
         #     line.strip().rsplit(" ", 1) if " " in line.strip() else (line.strip(), "")
         # )
-        argstr = text
+        # argstr = text
         # print("command:", command)
         # print("argstr:", argstr)
         # print("text:", text)
-        # options = [f"{cmd} " for cmd in shell.known_commands if cmd.startswith(command)]
+        options = [f"{cmd} " for cmd in shell.known_commands if cmd.startswith(text)]
         # if state < len(options):
         #     return options[state]
 
@@ -61,22 +61,23 @@ def make_completer(shell: Shell):
         #     #         return None
         #     # return " ".join(options)
         # else:
-        print("argstr:", argstr)
-        base_dir, partial_file = argstr.rsplit("/", 1)
+        if options == []:
+            print("argstr:", text)
+            base_dir, partial_file = text.rsplit("/", 1)
 
-        print("base_dir:", base_dir)
-        print("partial_file:", partial_file)
-        resolved_dir = (
-            base_dir
-            if argstr.startswith("/")
-            else os.path.join(shell._ctx.cwd, base_dir or "/")
-        )
-        try:
-            options = build_file_system_matches(resolved_dir, partial_file, line)[
-                "options"
-            ]
-        except OSError:
-            options = []
+            print("base_dir:", base_dir)
+            print("partial_file:", partial_file)
+            resolved_dir = (
+                base_dir
+                if text.startswith("/")
+                else os.path.join(shell._ctx.cwd, base_dir or "/")
+            )
+            try:
+                options = build_file_system_matches(resolved_dir, partial_file, line)[
+                    "options"
+                ]
+            except OSError:
+                options = []
 
         # if state == 0:
         #     if len(options) > 0 and cache["tab_count"] == 0:
