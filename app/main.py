a1baa77b2eb908d8a34f1143bb8ba4f8b02cd38f
kbaafi <kbaafi@gmail.com>
2026-06-08 07:22:18 +0300

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -122,7 +122,7 @@ def make_completer(shell: Shell):
         if state == 0 and len(options) > 1:
             sys.stdout.write("\a")
             sys.stdout.flush()
-        print("options:", options)
+        print("options:", options, "cwd:", shell._ctx.cwd, "read_cwd:", os.getcwd())
         return options[state] if state < len(options) else None
 
     return completer
