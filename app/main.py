d8101fccbeec3ffd72e38f8f16c09c322103e5a4
kbaafi <kbaafi@gmail.com>
2026-06-14 21:23:10 +0300

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -5,48 +5,7 @@ from pathlib import Path
 
 from .common import PROMPT, ParsedInput, tokenize_user_input
 from .shell import Shell
-
-
-def make_completer(shell: Shell):
-    def build_file_system_matches(base_dir, partial_name, cwd, text_prefix):
-        resolved_dir = (
-            base_dir
-            if base_dir.startswith("/")
-            else os.path.join(cwd, base_dir) if base_dir else cwd
-        )
-        options: list[str] = []
-        for entry in os.scandir(resolved_dir):
-            if entry.name.startswith(partial_name):
-                if entry.is_file():
-                    options.append(f"{text_prefix}{entry.name} ")
-                elif entry.is_dir():
-                    options.append(f"{text_prefix}{entry.name}/")
-        return sorted(options)
-
-    def completer(text: str, state: int):
-
-        line = readline.get_line_buffer()
-        cmd = line.split()[0] if line.split() else ""
-        arg = line.split()[-1] if line.split() and not line[-1].isspace() else text
-        base_dir, partial_file = arg.rsplit("/", 1) if "/" in arg else ("", arg)
-        text_prefix = text[: len(text) - len(partial_file)]
-        try:
-            options = build_file_system_matches(
-                base_dir, partial_file, shell._ctx.cwd, text_prefix
-            )
-        except OSError:
-            options = []
-
-        if options == [] and cmd:
-            options = [c for c in shell.known_commands if c.startswith(cmd)]
-            # print("arg:", arg, "line:", line, "text:", text)
-
-        if state == 0 and len(options) > 1:
-            sys.stdout.write("\a")
-            sys.stdout.flush()
-        return options[state] if state < len(options) else None
-
-    return completer
+from .shell_auto_complete import make_completer
 
 
 def main():
