4ca4c44061a0700ef5fb418d4b4c8d782258ef8f
kbaafi <kbaafi@gmail.com>
2026-06-14 16:22:15 +0300

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -26,44 +26,7 @@ def make_completer(shell: Shell):
         return sorted(options)
 
     def completer(text: str, state: int):
-        # nonlocal cache
 
-        # if state == 0:
-        # line = readline.get_line_buffer()
-        # print("line:", line)
-        # print("text:", text)
-
-        # print("text:", text)
-        # print("cache_text:", cache["text"])
-        # if len(tokens) == 0 or (len(tokens) == 1 and not line.endswith(" ")):
-        # command, argstr = (
-        #     line.strip().rsplit(" ", 1) if " " in line.strip() else (line.strip(), "")
-        # )
-        # argstr = text
-        # print("command:", command)
-        # print("argstr:", argstr)
-        # print("text:", text)
-        # options = [f"{cmd} " for cmd in shell.known_commands if cmd.startswith(text)]
-        # if state < len(options):
-        #     return options[state]
-
-        # if options == []:
-        # tokens = line.strip().split()
-        # partial = tokens[-1]
-        # if "/" not in partial:
-        #     options = build_file_system_matches(shell._ctx.cwd, partial, line)[
-        #         "options"
-        #     ]
-        #     # if len(options) > 0:
-        #     #     if cache["tab_count"] == 1:
-        #     #         print(cache["text"])
-        #     #         sys.stdout.write("\a")
-
-        #     #         sys.stdout.flush()
-        #     #         return None
-        #     # return " ".join(options)
-        # else:
-        #     if options == []:
         line = readline.get_line_buffer()
         arg = line.split()[-1] if line.split() and not line[-1].isspace() else text
         base_dir, partial_file = arg.rsplit("/", 1) if "/" in arg else ("", arg)
@@ -74,43 +37,7 @@ def make_completer(shell: Shell):
             )
         except OSError:
             options = []
-        # else:
-        #     print("options:", options)
 
-        # if state == 0:
-        #     if len(options) > 0 and cache["tab_count"] == 0:
-        #         cache["tab_count"] += 1
-        #         sys.stdout.write("\a")
-        #         sys.stdout.flush()
-        #         return None
-        #     elif len(options) > 0 and cache["tab_count"] > 0:
-        #         cache["tab_count"] += 1
-
-        #         out = " ".join(options)
-        #         sys.stdout.write(out)
-        #         sys.stdout.flush()
-        #         return "\n"
-
-        # if text != cache["text"]:
-        #     cache["tab_count"] = 0
-        #     cache["text"] = text
-        # cache["options"] = options
-        # cache["tab_count"] += 1
-
-        # if len(cache["options"]) == 0:
-        #     return None
-        # if len(cache["options"]) == 1:
-        #     return cache["options"][0]
-        # if cache["tab_count"] == 1:
-        #     sys.stdout.write("\a")
-        #     sys.stdout.flush()
-        #     return None
-        # if cache["tab_count"] > 1:
-        #     out = cache["base_dir"] + " ".join(cache["options"])
-        #     # out = tokens[0]
-        #     sys.stdout.write("")
-        #     sys.stdout.flush()
-        #     return out
         if state == 0 and len(options) > 1:
             sys.stdout.write("\a")
             sys.stdout.flush()
