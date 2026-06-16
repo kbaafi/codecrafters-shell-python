c28f3cba054f69725ed5c590eb02a41a8213db9f
kbaafi <kbaafi@gmail.com>
2026-06-17 00:33:35 +0300

    codecrafters submit [skip ci]

--- app/shell_auto_complete.py
+++ app/shell_auto_complete.py
@@ -28,22 +28,22 @@ def make_completer(shell: Shell):
         return sorted(options)
 
     def completer(text: str, state: int):
-
         line = readline.get_line_buffer()
         tokens = line.split()
         shell._refresh_executables()
-        cmd = tokens[0] if tokens else ""
+        cmd = tokens[0] if len(tokens) > 0 else ""
         is_typing_command = len(tokens) == 1 and not line[-1].isspace()
 
         if cmd in shell._ctx.completers:
-            sys.stdout.write(f"{text=}, {tokens=}")
+            # sys.stdout.write(f"{text=}, {tokens=}")
+            previous_text = tokens[-2] if len(tokens) >= 2 else ""
             script = shell._ctx.completers[cmd]
             env = os.environ.copy()
             env["COMP_LINE"] = line
             env["COMP_POINT"] = str(len(line))
             try:
                 result = subprocess.run(
-                    [script, cmd],
+                    [script, cmd, previous_text, text],
                     capture_output=True,
                     text=True,
                     env=env,
